af6bc779823ac7a23d176ab92d1610a00a8752f4
Anh Huy <onlinetran13@gmail.com>
2023-11-06 16:12:08 +0700

    ct449-project

---
+++ src/components/BaseSlider.vue
@@ -0,0 +1,65 @@
+<template>
+   <div class="slider-box">
+      <div class="slider-content">
+         <Splide :options="options" aria-label="My Favorite Images">
+            <SplideSlide>
+               <img src="http://cdn.shopify.com/s/files/1/0538/7249/2712/collections/Huffkins_Biscuits_1200x1200.jpg?v=1665651984" alt="Sample 2" />
+            </SplideSlide>
+            <SplideSlide>
+               <img src="https://cdn.shopify.com/s/files/1/0631/8804/1949/collections/collection-banner-2_d8208d36-220a-4459-91d6-174e9fb64f0e.jpg?v=1649063431&width=1500" alt="Sample 1" />
+            </SplideSlide>
+
+         </Splide>
+      </div>
+   </div>
+</template>
+
+<script>
+import { Splide, SplideSlide } from "@splidejs/vue-splide";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+   components: {
+      Splide,
+      SplideSlide,
+   },
+
+   setup() {
+      const options = {
+         rewind: true,
+         gap: "1rem",
+         autoplay: true,
+      };
+
+      return { options };
+   },
+});
+</script>
+
+<style scoped>
+.splide__slide img {
+   width: 100%;
+   height: 300px;
+   object-fit: cover; /* Bao phủ toàn bộ khung hình */
+   display: block;
+}
+.content {
+   margin-top: 5px;
+   position: absolute; /* Để đưa nội dung vào trong khung ảnh */
+   right: 10px;
+   bottom: 20px;
+   width: 400px;
+   text-align: justify;
+   background-color: #333;
+   padding: 7px 7px 12px;
+   border-radius: 3px;
+   opacity: 0.85;
+}
+.content a {
+   text-decoration: none;
+   color: white;
+   display: inline-block;
+   line-height: 24px;
+   text-transform: uppercase;
+}
+</style>
---
+++ src/components/ProductScreen.vue
@@ -0,0 +1,212 @@
+<template>
+   <div class="row">
+      <div class="product-grid col">
+         <div class="thumb">
+            <div class="image">
+               <img
+                  src="../images/home/1.jpg"
+                  alt="">
+            </div>
+         </div>
+      </div>
+      <div class="product-grid col">
+         <div class="thumb">
+            <div class="image">
+               <img
+                  src="../images/home/2.jpg"
+                  alt="">
+            </div>
+         </div>
+      </div>
+      <div class="product-grid col">
+         <div class="thumb">
+            <div class="image">
+               <img
+                  src="../images/home/3.jpg"
+                  alt="">
+            </div>
+         </div>
+      </div>
+      <div class="product-grid col">
+         <div class="thumb">
+            <div class="image">
+               <img
+                  src="../images/home/4.jpg"
+                  alt="">
+            </div>
+         </div>
+      </div>
+
+   </div>
+</template>
+
+<script>
+import ProductService from "../services/product.service";
+
+export default {
+   data() {
+      return {
+         products: [],
+         searchText: "",
+      };
+   },
+
+   computed: {
+      productStrings() {
+         return this.products.map((product) => {
+            const { name, description } = product;
+            return [name, description].join("");
+         });
+      },
+      filteredProducts() {
+         if (!this.searchText) return this.products;
+         return this.products.filter((_product, index) =>
+            this.productStrings[index].includes(this.searchText)
+         );
+      },
+      filteredProductsCount() {
+         return this.filteredProducts.length;
+      },
+   },
+   methods: {
+      async getAllProduct() {
+         try {
+            this.products = await ProductService.getAll();
+         } catch (error) {
+            console.log(error);
+         }
+      },
+      async addProductToCart(index) {
+         const saveLocalCart = JSON.parse(
+            localStorage.getItem("localProductCart") ?? "[]"
+         );
+         for (const item of saveLocalCart) {
+            if (item.id === this.products[index]._id) {
+               item.amount++;
+               const localProductCart = JSON.stringify(saveLocalCart);
+               localStorage.setItem("localProductCart", localProductCart);
+               alert(
+                  `Số lượng sản phẩm ${this.products[index].name} đã được cập nhật`
+               );
+               return;
+            }
+         }
+         const temp = {
+            id: this.products[index]._id,
+            name: this.products[index].name,
+            imgUrl: this.products[index].imgUrl,
+            price: this.products[index].price,
+            description: this.products[index].description,
+            amount: 1,
+         };
+         saveLocalCart.push(temp);
+         const localProductCart = JSON.stringify(saveLocalCart);
+         localStorage.setItem("localProductCart", localProductCart);
+         alert(
+            `Sản phẩm ${this.products[index].name} vừa được thêm vào giỏ hàng`
+         );
+      },
+   },
+
+   created() {
+      this.getAllProduct();
+   },
+};
+</script>
+
+<style scoped>
+.row {
+   padding-top: 30px;
+}
+
+#content {
+   min-height: 400x;
+}
+
+.product-category {
+   padding-top: 0px;
+}
+
+.product-name p {
+   color: red;
+}
+
+.header-content {
+   display: flex;
+   border-bottom: 3px solid #eee;
+   font-weight: normal;
+   font-size: 22px;
+}
+
+.search-btn {
+   margin-left: 550px;
+   padding-bottom: 5px;
+}
+
+.non-product {
+   margin-left: 450px;
+   margin-top: 15px;
+   font-size: 20px;
+   font-weight: bold;
+   color: blue;
+}
+
+.product-grid {
+   height: 180px;
+   position: relative;
+   text-align: center;
+   vertical-align: top;
+   padding: 7px;
+   margin-bottom: 15px;
+   border: 3px solid transparent;
+}
+
+.product-grid .thumb img {
+   object-fit: cover;
+   height: 150px;
+   box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+
+.product-grid .thumb .caption h4 a {
+   font-size: 13px;
+   line-height: 22px;
+   text-decoration: none;
+   padding: 5px;
+   color: #333;
+   margin: 0 auto;
+   width: 100%;
+   overflow: hidden;
+   text-overflow: ellipsis;
+   -webkit-line-clamp: 2;
+   display: -webkit-box;
+   -webkit-box-orient: vertical;
+}
+
+.product-grid:hover {
+   transform: scale(1.2);
+}
+
+.price {
+   color: #ff0000;
+   font-weight: bold;
+}
+
+.btn-primary {
+   background-color: #3e7cb4;
+}
+
+.botton-cart {
+   position: absolute;
+   bottom: 0;
+   left: calc(50% - 55px);
+}
+
+.btn-cart {
+   background-color: #3e7cb4;
+   color: #fff;
+   border-radius: 3px;
+}
+
+.btn-cart:hover {
+   background-color: #1760a0;
+}</style>
